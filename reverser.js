9d06c3b813450dd320fc6d76f4abe41349ae1712
Sarah Usher <sarahnusher@gmail.com>
2020-10-25 20:42:44 +0000

    reverse string test passing

--- reverser.js
+++ reverser.js
@@ -1,4 +1,16 @@
 function reverseString(stringToReverse) {
-    return stringToReverse;
+    let stack = [];
+
+    for (let i = 0; i < stringToReverse.length; i++){
+        stack.push(stringToReverse[i]);
+    }
+
+    let reversedString = '';
+    for (let i = 0; i < stringToReverse.length; i++){
+        let character = stack.pop();
+        reversedString += character;
+    }
+
+    return reversedString;
 }
 module.exports = reverseString;
